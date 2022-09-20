42753474b4ecca4001ccd656b13dade3fbbe2cf8
Philipp Schneider <47689073+philipp-tailor@users.noreply.github.com>
2022-09-20 17:18:29 +0200

    fix: treat BeB as component
    Because it is not a standalone page.
    
    Also only access client-side-only `window` object on the server side.
    For more advanced approaches to implement this, see:
    https://nextjs.org/docs/advanced-features/dynamic-import#with-no-ssr

--- components/BirdBug.jsx
+++ components/BirdBug.jsx
@@ -1,23 +1,23 @@
-import Script from 'next/script';
+import Script from "next/script";
 
 export function BirdBug() {
   const destinationLookup = {
-    default: '14',
-    hit: '8b',
-    marketing: '89',
+    default: "14",
+    hit: "8b",
+    marketing: "89",
   };
 
-  const hitPaths = ['/', '/dashboard/vercel/domains'];
+  const hitPaths = ["/", "/dashboard/vercel/domains"];
   const marketingPaths = [
-    '/features/previews',
-    '/features/infrastructure',
-    '/features/edge-functions',
-    '/customers',
-    '/contact/sales',
+    "/features/previews",
+    "/features/infrastructure",
+    "/features/edge-functions",
+    "/customers",
+    "/contact/sales",
   ];
 
   function getDestination() {
-    const pathname = window.location.pathname;
+    const pathname = typeof window !== "undefined" && window.location.pathname;
     if (hitPaths.includes(pathname)) {
       return destinationLookup.hit;
     }
@@ -33,7 +33,7 @@ export function BirdBug() {
         __html: `
       ;(function(){const birdeatsbug=(window.birdeatsbug=window.birdeatsbug||[]);if(birdeatsbug.initialize)return;if(birdeatsbug.invoked){if(window.console&&console.error){console.error('birdeatsbug snippet included twice.')}return}birdeatsbug.invoked=true;birdeatsbug.methods=['setOptions','startSession','resumeSession','startRecording','stopRecording','stopSession','uploadSession','deleteSession'];birdeatsbug.factory=function(method){return function(){var args=Array.prototype.slice.call(arguments);args.unshift(method);birdeatsbug.push(args);return birdeatsbug}};for(var i=0;i<birdeatsbug.methods.length;i++){var key=birdeatsbug.methods[i];birdeatsbug[key]=birdeatsbug.factory(key)}birdeatsbug.load=function(){var script=document.createElement('script');script.type='module';script.async=true;script.src='https://sdk.birdeatsbug.com/latest/core.js';var first=document.getElementsByTagName('script')[0];first.parentNode.insertBefore(script,first);const style=document.createElement('link');style.rel='stylesheet';style.type='text/css';style.href='https://sdk.birdeatsbug.com/latest/style.css';first.parentNode.insertBefore(style,first)};birdeatsbug.load();
       window.birdeatsbug.setOptions({
-        publicAppId:'${getDestination()}',
+        publicAppId: '${getDestination()}',
         ui: {
           submitConfirmationScreen: {sessionLink: false},
         },
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { BirdBug } from './BirdBug';
+import { BirdBug } from "../components/BirdBug";
 
 export default function Home() {
   return (
